60b332687f82eb14500863894b67429a0b749cf7
dhkim <kdh4501@hicare.net>
2023-07-28 16:42:39 +0900

    feat 구글 계정으로 firebase Auth 계정 생성 추가

--- app/src/main/java/com/dhkim/mvvminsta/login/LoginActivity.kt
+++ app/src/main/java/com/dhkim/mvvminsta/login/LoginActivity.kt
@@ -3,14 +3,16 @@ package com.dhkim.mvvminsta.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.dhkim.mvvminsta.R
 import com.dhkim.mvvminsta.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var auth : FirebaseAuth
     lateinit var binding : ActivityLoginBinding
     val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,6 @@ class LoginActivity : AppCompatActivity() {
         binding.viewModel = loginViewModel
         binding.activity = this
         binding.lifecycleOwner = this
-        auth = FirebaseAuth.getInstance()
         setObserve()
     }
 
@@ -37,23 +38,22 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun loginWithSignupEmail() {
-        auth.createUserWithEmailAndPassword(loginViewModel.id.value.toString(), loginViewModel.password.value.toString())
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    loginViewModel.showInputNumberActivity.value = true
-                } else {
-                    // id 있을 경우
-
-                }
-            }
-    }
-
     fun findId() {
         loginViewModel.showFindIdActivity.value = true
     }
 
     fun loginFacebook() {
 
+    }
+
+    // 구글 로그인이 성공한 결과값 받는 함수
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+         // 로그인한 사용자 정보를 암호화한 값
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)
     }
 }
