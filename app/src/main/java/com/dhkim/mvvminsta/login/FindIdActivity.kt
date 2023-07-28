b39440f3ef1c5875c039e4479d56b695c7a6c5da
dhkim <kdh4501@hicare.net>
2023-07-28 17:53:03 +0900

    feat firebaseAuth 아이디 찾기, 비번 초기화 구현

--- app/src/main/java/com/dhkim/mvvminsta/login/FindIdActivity.kt
+++ app/src/main/java/com/dhkim/mvvminsta/login/FindIdActivity.kt
@@ -2,11 +2,28 @@ package com.dhkim.mvvminsta.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.dhkim.mvvminsta.R
+import com.dhkim.mvvminsta.databinding.ActivityFindIdBinding
 
 class FindIdActivity : AppCompatActivity() {
+    lateinit var binding : ActivityFindIdBinding
+    val findIdViewModel : FindIdViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_find_id)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_find_id)
+        binding.activity = this
+        binding.viewModel = findIdViewModel
+        binding.lifecycleOwner = this
+        setObserve()
+    }
+    fun setObserve() {
+        findIdViewModel.toastMessage.observe(this) {
+            if (it.isNotEmpty()) {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
