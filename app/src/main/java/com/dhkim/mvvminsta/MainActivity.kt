1100d907ca47160a275b45ce0dfcf16e55b73f4a
dhkim <kdh4501@hicare.net>
2023-07-31 10:14:00 +0900

    feat Main의 bottom Navi랑 fragment 연결

--- app/src/main/java/com/dhkim/mvvminsta/MainActivity.kt
+++ app/src/main/java/com/dhkim/mvvminsta/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.dhkim.mvvminsta
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.dhkim.mvvminsta.databinding.ActivityMainBinding
+import com.dhkim.mvvminsta.fragment.AlarmFragment
+import com.dhkim.mvvminsta.fragment.DetailViewFragment
+import com.dhkim.mvvminsta.fragment.GridFragment
+import com.dhkim.mvvminsta.fragment.UserFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.action_home -> {
+                    var f = DetailViewFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, f).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_search -> {
+                    var f = GridFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, f).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_favorite_alarm -> {
+                    var f = AlarmFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, f).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_account -> {
+                    var f = UserFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content, f).commit()
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
 }
